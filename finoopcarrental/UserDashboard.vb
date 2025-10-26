f62a5f27252b91f254a88136e1e7d63c0792596d
Michaellitang <michaellitang0426@gmail.com>
2025-10-27 05:05:55 +0800

    UserDashboard

--- finoopcarrental/UserDashboard.vb
+++ finoopcarrental/UserDashboard.vb
@@ -1,3 +1,14 @@
 ﻿Public Class UserDashboard
+    Private Sub UserDashboard_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim userDashboard As New UserDashboardPage2()
+        UserDashboardPage2.Show()
+    End Sub
 End Class
---
+++ finoopcarrental/UserDashboardPage2.vb
@@ -0,0 +1,9 @@
+﻿Public Class UserDashboardPage2
+    Private Sub UserDashboardPage2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub PictureBox4_Click(sender As Object, e As EventArgs) Handles PictureBox4.Click
+
+    End Sub
+End Class
